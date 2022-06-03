b79a9b86fc815e0f0fb7fb9283dfaa84ca794d9a
Murat Karagözgil <m.karagozgil@gmail.com>
2022-06-03 11:54:04 +0300

    Changed task name of bonus activity

--- activity-lib/src/main/java/org/example/MoneyTransferWorkflowImpl.java
+++ activity-lib/src/main/java/org/example/MoneyTransferWorkflowImpl.java
@@ -26,13 +26,20 @@ public class MoneyTransferWorkflowImpl implements MoneyTransferWorkflow {
         // Temporal retries failures by default, this is simply an example.
         .setRetryOptions(retryoptions).build();
 
+    private final ActivityOptions bonusActivityOptions = ActivityOptions.newBuilder()
+        // Timeout options specify when to automatically timeout Activities if the process is taking too long.
+        .setStartToCloseTimeout(Duration.ofSeconds(5))
+        // Optionally provide customized RetryOptions.
+        // Temporal retries failures by default, this is simply an example.
+        .setTaskQueue("BONUS_CHECK_TASK_QUEUE").setRetryOptions(retryoptions).build();
+
     // ActivityStubs enable calls to methods as if the Activity object is local, but actually perform an RPC.
     private final Map<String, ActivityOptions> perActivityMethodOptions = new HashMap<>() {{
         put(WITHDRAW, ActivityOptions.newBuilder().setHeartbeatTimeout(Duration.ofSeconds(5)).build());
     }};
 
     private final AccountActivity account = Workflow.newActivityStub(AccountActivity.class, defaultActivityOptions, perActivityMethodOptions);
-    private final BonusActivity bonus = Workflow.newActivityStub(BonusActivity.class, defaultActivityOptions, perActivityMethodOptions);
+    private final BonusActivity bonus = Workflow.newActivityStub(BonusActivity.class, bonusActivityOptions, perActivityMethodOptions);
 
     // The transfer method is the entry point to the Workflow.
     // Activity method executions can be orchestrated here or from within other Activity methods.
